ab741ede0870db8703493487f4fbd51b3aa9611f
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-01-11 18:42:22 +0300

    Initial Commit

---
+++ swiftuiall/swiftuiallApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiallApp.swift
+//  swiftuiall
+//
+//  Created by Furkan Cingöz on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
