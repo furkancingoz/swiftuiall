8daf1f0c0decbde13d3e4fc3ea88178d43f6ff1e
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-01-11 19:11:46 +0300

    how to use inits and enums in swift ui

---
+++ swiftuiall/InitializerAndEnums.swift
@@ -0,0 +1,52 @@
+//
+//  Initializer.swift
+//  swiftuiall
+//
+//  Created by Furkan Cingöz on 11.01.2024.
+//
+
+import SwiftUI
+
+struct InitializerAndEnums: View {
+  let applecount: Int
+  let fruit: String
+  let backgroundColor: Color
+
+  init(applecount: Int, fruit: Fruit) {
+    self.applecount = applecount
+//    self.fruit = fruit
+//    self.backgroundColor = backgroundColor
+    
+    if fruit == .apple {
+      self.backgroundColor = .red
+      self.fruit = "Apple"
+    } else {
+      self.backgroundColor = .orange
+      self.fruit = "Orange"
+    }
+  }
+
+  enum Fruit {
+    case apple
+    case orange
+  }
+
+    var body: some View {
+      VStack(spacing: 10){
+        Text("\(applecount)")
+          .font(.largeTitle)
+          .foregroundStyle(.white)
+
+        Text(fruit)
+          .font(.headline)
+          .foregroundStyle(.white)
+      }
+      .frame(width: 150,height: 150)
+      .background(backgroundColor)
+      .cornerRadius(10)
+    }
+}
+
+#Preview {
+  InitializerAndEnums(applecount: 10, fruit: .orange)
+}
