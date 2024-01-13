8f5536376239a1fdb930b8ecf7880d341443340c
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-01-13 23:11:16 +0300

    add buttons

--- swiftuiall/ContentView.swift
+++ swiftuiall/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-        
+          InitializerAndEnums(applecount: 3, fruit: .apple)
         }
         .padding()
     }
